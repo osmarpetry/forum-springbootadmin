f1d16340f71aa5a659853088e139602be0fcc2a7
osmarpetry <osmarpetry@gmail.com>
2020-03-19 11:46:52 -0300

    spring boot jdk8 spring web

---
+++ src/main/java/com/forum/sprintbootadmin/SprintBootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.forum.sprintbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootAdminApplication.class, args);
+	}
+
+}
